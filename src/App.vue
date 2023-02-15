271e105e612b82a8132b0b4d446bdb5e1db112bf
olo <olo@O-L-O.OLO>
2023-02-15 12:58:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="dark">
+    <main
+      class="w-full h-full absolute dark:bg-slate-800 flex items-center justify-center"
+    >
+      <h1 class="dark:text-white">Hello World</h1>
+    </main>
+  </div>
+</template>
